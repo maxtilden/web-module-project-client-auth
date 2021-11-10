545421a0e6b5992319ff457de33e8d510ace8fbb
Max Tilden <maxtilden@gmail.com>
2021-11-09 23:43:49 -0800

    initialcommit

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+//initial commit
 ReactDOM.render(
   <App />,
   document.getElementById('root')
